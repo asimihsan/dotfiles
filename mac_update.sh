a0296f12f6093f039a6a5712c3ac36f4019116ea
Asim Ihsan <ssh@sshemail.ihsan.io>
2022-09-23 12:19:10 -0700

    mac_update - cache rust deps

--- mac_update.sh
+++ mac_update.sh
@@ -7,8 +7,13 @@ sudo echo starting...
 sudo softwareupdate --download --all --agree-to-license
 brew update
 brew upgrade
-rustup update stable
 flutter upgrade
 conda update --all --yes
+
+rustup update stable
+cargo install cargo-prefetch
+cargo prefetch --top-deps=200
+cargo prefetch --top-downloads=200
+
 brew doctor
 flutter doctor
