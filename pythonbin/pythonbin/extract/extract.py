45877fe3cec79d2016918363837cdbe2d6b431a6
Asim Ihsan <ssh@sshemail.ihsan.io>
2024-05-22 20:37:14 -0400

    wip

--- pythonbin/pythonbin/extract/extract.py
+++ pythonbin/pythonbin/extract/extract.py
@@ -1,41 +1,80 @@
+import collections
 import os
 import sys
+from pathlib import Path
 
-from unstructured.partition.auto import partition
+from unstructured.partition.auto import partition, partition_pdf, partition_epub
+from unstructured.cleaners.core import clean
 
 
-def extract(path: str) -> str:
-    elements = partition(
+def extract(path: Path) -> str:
+    print(f"Extracting {path}...")
+
+    ext = path.suffix.lower()
+    if ext == ".pdf":
+        method = partition_pdf
+    elif ext == ".epub":
+        method = partition_epub
+    else:
+        method = partition
+
+    elements = method(
         filename=path,
         strategy="hi_res",
         # extract_image_block_types=["Image"],
         # extract_image_block_output_dir=os.path.dirname(path),
         request_timeout=10,
     )
-    return "\n\n".join(str(el) for el in elements)
+    return "\n\n".join(
+        clean(
+            str(el),
+            bullets=False,
+            extra_whitespace=True,
+            dashes=True,
+            trailing_punctuation=True,
+        )
+        for el in elements
+    )
 
 
-def run_main(path: str, write: bool = False):
-    # Validate input file path
-    if not os.path.exists(path):
-        print(f"Error: File {path} does not exist.")
-        sys.exit(1)
+def run_main(paths: list[str], write: bool = False):
+    deque = collections.deque((Path(p) for p in paths))
 
-    if not os.path.isfile(path):
-        print(f"Error: {path} is not a file.")
-        sys.exit(1)
+    while deque:
+        path = Path(deque.popleft())
 
+        if not path.exists():
+            print(f"Error: File {path} does not exist.")
+            continue
+
+        if path.is_dir():
+            for p in path.iterdir():
+                if p.is_file():
+                    deque.append(p)
+            continue
+
+        if not path.is_file():
+            print(f"Error: {path} is not a file.")
+            continue
+
+        do_extract(path, write=write)
+
+
+def do_extract(path: Path, write: bool = False):
     output = extract(path)
 
-    # Validate output file path
-    if write:
-        output_dir = os.path.dirname(path)
-        if not os.path.exists(output_dir):
-            print(f"Error: Directory {output_dir} does not exist.")
-            sys.exit(1)
-
-        output_path = os.path.splitext(path)[0] + ".txt"
-        with open(output_path, "w") as f:
-            f.write(output)
-    else:
+    if not write:
         print(output)
+        return
+
+    output_dir = path.parent
+    if not output_dir.exists():
+        print(f"Error: Directory {output_dir} does not exist.")
+        sys.exit(1)
+
+    # keep original extension, add .txt on end
+    output_path = output_dir / f"{path}.txt"
+    print(f"Writing to {output_path}...")
+
+    with open(output_path, "w") as f:
+        f.write(output)
