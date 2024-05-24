05d1a374b2c6d6d520bb9a2c5f961ae403c3a603
Asim Ihsan <ssh@sshemail.ihsan.io>
2024-05-23 22:25:53 -0400

    wip

--- pythonbin/pythonbin/epub/utils.py
+++ pythonbin/pythonbin/epub/utils.py
@@ -1,4 +1,47 @@
+from inscriptis import get_annotated_text, ParserConfig
 import spacy
+from intervaltree import IntervalTree
+
+
+class HTMLToText:
+    def __init__(self, model_name="en_core_web_trf"):
+        self.spacy_model = SpacyModel(model_name)
+
+    def html_to_sentences(self, html: str) -> list[str]:
+        annotation_rules = {"pre": ["pre"]}
+        config = ParserConfig(annotation_rules=annotation_rules)
+        annotated_text = get_annotated_text(html, config)
+        text = annotated_text["text"]
+
+        annotations = annotated_text["label"]
+        pre_intervals = self.build_interval_tree(annotations)
+
+        doc = self.spacy_model.process_text(text)
+        sentences = self.merge_sentences_with_pre(doc, pre_intervals)
+
+        return sentences
+
+    def build_interval_tree(self, annotations) -> IntervalTree:
+        tree = IntervalTree()
+        for start, end, label in annotations:
+            if label == "pre":
+                tree[start:end] = label
+        return tree
+
+    def merge_sentences_with_pre(self, doc, pre_intervals) -> list[str]:
+        sentences = []
+        current_sentence = []
+        for sent in doc.sents:
+            if pre_intervals.overlaps(sent.start_char, sent.end_char):
+                if current_sentence:
+                    sentences.append(" ".join(current_sentence))
+                    current_sentence = []
+                sentences.append(sent.text.strip())
+            else:
+                current_sentence.append(sent.text.strip())
+        if current_sentence:
+            sentences.append(" ".join(current_sentence))
+        return sentences
 
 
 class SpacyModel:
