300bb103f16e57c9a33b1bf41d8e07b09b33c3f9
Asim Ihsan <ssh@sshemail.ihsan.io>
2024-05-23 22:25:53 -0400

    wip

--- pythonbin/pythonbin/epub/parser.py
+++ pythonbin/pythonbin/epub/parser.py
@@ -1,16 +1,17 @@
 import concurrent.futures
 import collections
 import pathlib
+import os
 
 from bs4 import BeautifulSoup
 
 import pythonbin.epub.vebooklib as vebooklib
 from .models import Ebook, Chapter, TocItem
-from .utils import SpacyModel
+from .utils import SpacyModel, HTMLToText
 
 
 def read_epub(file_path: pathlib.Path) -> Ebook:
-    book = vebooklib.read_epub(str(file_path))
+    book = vebooklib.read_epub(str(file_path), options=dict(ignore_ncx=True))
     title = book.get_metadata("DC", "title")[0][0]
     ebook = Ebook(title=title)
 
@@ -48,24 +49,26 @@ def _extract_toc(book) -> list[TocItem]:
     return toc_items
 
 
+def init_html_to_text():
+    global html_to_text
+    html_to_text = HTMLToText()
+
+
 def _parse_chapter(toc_item: TocItem, book: vebooklib.EpubBook) -> Chapter:
     print(f"Extracting chapter: {toc_item.title}")
 
     book_item: vebooklib.EpubHtml = book.get_item_with_href(toc_item.filename)  # type: ignore
 
     content = book_item.get_body_content()
-    soup = BeautifulSoup(content, "lxml")
+    soup = BeautifulSoup(content, "lxml", from_encoding="utf-8")
 
     clean_content = soup.prettify()
     chapter = Chapter(title=toc_item.title, raw_content=clean_content)
 
+    global html_to_text
+    chapter.sentences = html_to_text.html_to_sentences(clean_content)
+
     return chapter
-
-
-def _tokenize_sentences(text: str) -> list[str]:
-    doc = nlp(text)
-    sentences = [sent.text for sent in doc.sents]
-    return sentences
 
 
 def _extract_chapters(book: vebooklib.EpubBook, toc_items: list[TocItem]) -> list[Chapter]:
@@ -78,13 +81,8 @@ def _extract_chapters(book: vebooklib.EpubBook, toc_items: list[TocItem]) -> lis
         if toc_item.filename not in seen_filenames and not seen_filenames.add(toc_item.filename)
     ]
 
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    workers = max(os.cpu_count() - 1, 1)  # type: ignore
+    with concurrent.futures.ThreadPoolExecutor(max_workers=workers, initializer=init_html_to_text) as executor:
         chapters = list(executor.map(_parse_chapter, unique_toc_items, [book] * len(unique_toc_items)))
-
-    spacy_model = SpacyModel()
-    for chapter in chapters:
-        doc = spacy_model.process_text(chapter.raw_content)
-        sentences = [sent.text for sent in doc.sents]
-        chapter.sentences = sentences
 
     return chapters
