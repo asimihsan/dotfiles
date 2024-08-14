a73685294ec4ad887079bb531bc39a01de0f403f
Asim Ihsan <asim.ihsan@level.co>
2024-08-14 09:52:40 -0400

    wip

--- mac_update.sh
+++ mac_update.sh
@@ -43,20 +43,14 @@ sudo softwareupdate --download --all --agree-to-license
 brew update
 brew upgrade
 
-(cd ~/.dotfiles && git pull --rebase)
-
 if command -v flutter; then
     flutter upgrade --force
 fi
 
 rustup update stable
-cargo install cargo-prefetch
-cargo prefetch --top-deps=200
-cargo prefetch --top-downloads=200
 
-npm install --ugprade npm
-npm install --upgrade aws-cdk
-npm install --upgrade '@githubnext/github-copilot-cli'
+~/bin/install-npm-global.sh
+npm update -g
 
 brew doctor
 if command -v flutter; then
