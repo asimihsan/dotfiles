3b57645be127ff290f8f08b5116aeabcdfc3080c
Asim Ihsan <asim.ihsan@level.co>
2024-09-12 11:00:06 -0400

    wip

--- mac_update.sh
+++ mac_update.sh
@@ -11,19 +11,19 @@ echo starting...
 
 update_dotfiles() {
     local dotfiles_dir="$HOME/.dotfiles"
-    
+
     if [[ ! -d "$dotfiles_dir" ]]; then
         echo "No ~/.dotfiles directory found, skipping git update"
         return 0
     fi
-    
+
     cd "$dotfiles_dir" || {
         echo "Failed to change directory to $dotfiles_dir"
         return 1
     }
-    
+
     echo "Updating dotfiles repository..."
-    
+
     if ! git town sync; then
         echo "Failed to git town sync"
         return 1
@@ -33,11 +33,14 @@ update_dotfiles() {
 update_dotfiles
 
 "$HOME"/.dotfiles/install
+cp $(devbox global path)/devbox.{lock,json} ~/.dotfiles/chezmoi/dot_local/share/devbox/global/default/
+
 chezmoi update
 chezmoi upgrade
 chezmoi apply
 
 devbox global update
+cp $(devbox global path)/devbox.{lock,json} ~/.dotfiles/chezmoi/dot_local/share/devbox/global/default/
 
 sudo softwareupdate --download --all --agree-to-license
 brew update
