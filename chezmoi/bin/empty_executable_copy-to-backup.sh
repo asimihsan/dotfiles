b8c4eb3563bd2fba0ce5ce9c2e8319f27ba0a49e
Asim Ihsan <asim.ihsan@level.co>
2024-10-13 16:39:21 -0400

    wip

--- chezmoi/bin/empty_executable_copy-to-backup.sh
+++ chezmoi/bin/empty_executable_copy-to-backup.sh
@@ -5,3 +5,4 @@ set -euo pipefail
 cp $(devbox global path)/devbox.{lock,json} ~/.dotfiles/chezmoi/dot_local/share/devbox/global/default/
 cp ~/.config/zed/settings.json ~/.dotfiles/chezmoi/dot_config/zed/private_settings.json
 cp ~/.config/zed/tasks.json ~/.dotfiles/chezmoi/dot_config/zed/private_tasks.json
+cp ~/.config/karabiner/karabiner.json ~/.dotfiles/chezmoi/dot_config/private_karabiner/private_karabiner.json
