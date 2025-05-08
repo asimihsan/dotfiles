1e2018770db5015c7e8ed4819bb429d13c7b5199
Asim Ihsan <asim.ihsan@level.co>
2025-05-08 16:49:56 -0400

    wip

--- chezmoi/bin/empty_executable_copy-to-backup.sh
+++ chezmoi/bin/empty_executable_copy-to-backup.sh
@@ -2,13 +2,17 @@
 
 set -euo pipefail
 
-chezmoi add "$(devbox global path)"/devbox.json
-chezmoi add "$(devbox global path)"/devbox.lock
-chezmoi add ~/.config/zed/settings.json
-chezmoi add ~/.config/zed/tasks.json
-chezmoi add ~/.config/karabiner/karabiner.json
-chezmoi add ~/.config/kitty/kitty.conf
-chezmoi add --template ~/.zshrc
-chezmoi add ~/Library/Preferences/com.googlecode.iterm2.plist
-chezmoi add ~/Library/Preferences/com.googlecode.iterm2.private.plist
 chezmoi add --encrypt ~/.claude.json
+chezmoi add --template ~/.zshrc
+chezmoi re-add "$(devbox global path)"/devbox.json
+chezmoi re-add "$(devbox global path)"/devbox.lock
+chezmoi re-add ~/.claude.json
+chezmoi re-add ~/.config/karabiner/karabiner.json
+chezmoi re-add ~/.config/kitty/kitty.conf
+chezmoi re-add ~/.config/mise/config.toml
+chezmoi re-add ~/.config/zed/settings.json
+chezmoi re-add ~/.config/zed/settings.json
+chezmoi re-add ~/.config/zed/tasks.json
+chezmoi re-add ~/bin/idea
+chezmoi re-add ~/Library/Preferences/com.googlecode.iterm2.plist
+chezmoi re-add ~/Library/Preferences/com.googlecode.iterm2.private.plist
